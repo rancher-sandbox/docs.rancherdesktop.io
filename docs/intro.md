d4901f4b9d1577f4d531b946003faafa8a12b73d
Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>
2022-01-06 09:15:04 -0800

    first commit
    Signed-off-by: Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>

---
+++ docs/intro.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# About
+
+Rancher Desktop is an open-source desktop application for Mac, Windows and Linux. It provides Kubernetes and container management. You can choose the version of Kubernetes you want to run. You can build, push, pull, and run container images. The container images you build can be run by Kubernetes immediately without the need for a registry.
---
+++ docusaurus.config.js
@@ -0,0 +1,80 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Rancher Desktop Docs',
+  tagline: 'Rancher Desktop Docs',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'SUSE LLC', // Usually your GitHub org/user name.
+  projectName: 'rancher-desktop', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/rancher-sandbox/rancher-desktop-docs/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        logo: {
+          alt: 'Rancher Desktop Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            href: 'https://rancherdesktop.io/',
+            label: 'Rancher Desktop Home',
+            target: '_self',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/rancher-sandbox/rancher-desktop-docs/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true,
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} SUSE. Rancher Desktop is an open source project of the SUSE Rancher Engineering group. This docs site is built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+    plugins: [
+      [require.resolve('docusaurus-lunr-search'),
+          {
+              indexBaseUrl: true
+          }
+      ]
+    ],
+};
+
+module.exports = config;
